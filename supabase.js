7677d777dd8009987d422d99cc372a5045a90cd0
yesanovice <arkokazi08@gmail.com>
2026-05-11 03:44:41 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://ezoidfgpazczkhrydozeg.supabase.co',
-  'sb_publishable_lvlnxVP0wFDqWwcbK9qBqg_U6C008mr'
-)
+const url = 'https://ezoidfgpazczkhrydozeg.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6b2lkZmdwYXpja2hyeWRvemVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MzA3MDMsImV4cCI6MjA5NDAwNjcwM30.mjFbPp0-gh5CBFkZm7Tv6E_5BloBMVlQCDkcAH0iasI'
+
+export const supabase = createClient(url, key)
